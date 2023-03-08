91fa0105b9719c3db8afdbfc0f18d18b1e5a6ed8
khoavo13 <vandangkhoa.vo@email.com>
2023-03-08 23:30:49 +0700

    dynamic topic kafka

---
+++ websocket-redis-master/src/main/java/com/example/websocketredis/controller/DynamicTopicController.java
@@ -0,0 +1,25 @@
+package com.example.websocketredis.controller;
+
+import com.example.websocketredis.config.KafkaDynamicTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DynamicTopicController {
+    @Autowired
+    private KafkaDynamicTopic kafkaDynamicTopic;
+    @PostMapping("/createTopic")
+    public ResponseEntity<String> createTopic(@RequestParam String topic){
+        try {
+            kafkaDynamicTopic.createTopicDynamic(topic);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ResponseEntity<>("Topic " + topic + " created successfully.", HttpStatus.OK);
+    }
+}
